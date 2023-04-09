c54433f412c2c417825a1d555683b84c0d5400ef
opsEpic <130291335+opsEpic@users.noreply.github.com>
2023-04-09 20:39:14 +0800

    更新
    1.机器人会@你
    2.-set设定关键字
    3.-show显示关键字

--- main.go
+++ main.go
@@ -2,114 +2,133 @@ package main
 
 import (
 	"bytes"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"github.com/sirupsen/logrus"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 )
 
-type req struct {
-	Network bool   `json:"network"`
-	Prompt  string `json:"prompt"`
-	UserId  string `json:"userId"`
+var character sync.Map
+
+func myTrim(s string, cut string) string {
+	for i := range s {
+		if len(s[i:]) < len(cut) {
+			return s
+		}
+
+		if s[i:i+len(cut)] == cut {
+			add := ""
+			if i+len(cut) < len(s) {
+				add = s[i+len(cut):]
+			}
+			return s[:i] + add
+		}
+	}
+	return s
 }
 
-type send struct {
-	Nickname string `json:"nickname"`
-}
+func send2gpt3method1(s string, id int) string {
+	type req struct {
+		Network bool   `json:"network"`
+		Prompt  string `json:"prompt"`
+		UserId  string `json:"userId"`
+	}
 
-type strData struct {
-	Message_type string `json:"message_type"`
-	Self_id      int    `json:"self_id"`
-	User_id      int    `json:"user_id"`
-	Group_id     int    `json:"group_id"`
-
-	Sender send `json:"sender"`
-
-	Message string `json:"message"`
-}
-
-type rsp struct {
-	Message_type string `json:"message_type"`
-	User_id      int    `json:"user_id"`
-	Group_id     int    `json:"group_id"`
-	Message      string `json:"message"`
-}
-
-func send2gpt(s string, id int) string {
 	w := req{false, s, "#/chat/" + fmt.Sprint(id)}
 	j, _ := json.Marshal(w)
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	client := &http.Client{Transport: tr}
-	r, err := client.Post("https://cbjtestapi.binjie.site:7777/api/generateStream", "application/json", bytes.NewReader(j))
+	for {
+		r, _ := http.Post("https://cbjtestapi.binjie.site:7777/api/generateStream", "application/json", bytes.NewReader(j))
+		ans, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close()
 
-	if err != nil {
-		logrus.Error(err)
-		return err.Error()
+		if !strings.Contains(string(ans), `https://chat1.yqcloud.top`) {
+			return string(ans)
+		}
 	}
-	defer r.Body.Close()
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		logrus.Error(err)
-		return err.Error()
-	}
-	return string(body)
 }
 
 func receive(w http.ResponseWriter, r *http.Request) {
-
-	ans := make([]byte, 0)
-	for {
-		temp := make([]byte, 256)
-		n, _ := r.Body.Read(temp)
-		if n == 0 {
-			break
-		}
-
-		ans = append(ans, temp[:n]...)
+	type fromQQ struct {
+		Message_type string `json:"message_type"`
+		Self_id      int    `json:"self_id"`
+		User_id      int    `json:"user_id"`
+		Group_id     int    `json:"group_id"`
+		Message      string `json:"message"`
+	}
+	type toQQ struct {
+		Message_type string `json:"message_type"`
+		User_id      int    `json:"user_id"`
+		Group_id     int    `json:"group_id"`
+		Message      string `json:"message"`
 	}
 
-	var data strData
+	ans, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+
+	var data fromQQ
 	json.Unmarshal(ans, &data)
 
-	//fmt.Println(data)
-
 	go func() {
-		for {
-			if data.Message_type == "group" {
-				if !strings.Contains(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]") {
-					return
-				}
-
-				fmt.Println("qu:"+strings.Trim(strings.Trim(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"] "), "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]"), data.Group_id)
-				s := send2gpt(
-					strings.Trim(strings.Trim(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"] "), "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]"),
-					data.Group_id,
-				)
-				fmt.Println("re:" + s)
-
-				if !strings.Contains(s, `https://chat1.yqcloud.top`) {
-					re, _ := json.Marshal(rsp{Message_type: "group", Group_id: data.Group_id, Message: s})
-					http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
-					return
-				}
-			} else if data.Message_type == "private" {
-				fmt.Println("qu:"+data.Message, data.User_id)
-				s := send2gpt(data.Message, data.User_id)
-				fmt.Println("re:" + s)
-
-				if !strings.Contains(s, `https://chat1.yqcloud.top`) {
-					re, _ := json.Marshal(rsp{Message_type: "private", User_id: data.User_id, Message: s})
-					http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
-					return
-				}
+		if data.Message_type == "group" {
+			if strings.Contains(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"] ") {
+				data.Message = myTrim(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"] ")
+			} else if strings.Contains(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]") {
+				data.Message = myTrim(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]")
+			} else {
+				return
 			}
+
+			s := "[CQ:at,qq=" + fmt.Sprint(data.User_id) + "] "
+			if strings.Contains(data.Message, "-set ") {
+				data.Message = myTrim(data.Message, "-set ")
+				character.Store(data.Group_id, data.Message)
+				return
+			} else if strings.Contains(data.Message, "-show") {
+				v, ok := character.Load(data.Group_id)
+				if ok {
+					s += v.(string)
+				}
+			} else {
+				v, ok := character.Load(data.Group_id)
+				if ok {
+					data.Message += v.(string)
+				}
+				s += send2gpt3method1(data.Message, data.Group_id)
+			}
+
+			fmt.Println("qu:" + data.Message)
+			fmt.Println("re:" + s)
+
+			re, _ := json.Marshal(toQQ{Message_type: "group", Group_id: data.Group_id, Message: s})
+			http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
+
+		} else if data.Message_type == "private" {
+			s := ""
+			if strings.Contains(data.Message, "-set ") {
+				data.Message = myTrim(data.Message, "-set ")
+				character.Store(data.User_id, data.Message)
+				return
+			} else if strings.Contains(data.Message, "-show") {
+				v, ok := character.Load(data.User_id)
+				if ok {
+					s += v.(string)
+				}
+			} else {
+				v, ok := character.Load(data.User_id)
+				if ok {
+					data.Message += v.(string)
+				}
+				s += send2gpt3method1(data.Message, data.User_id)
+			}
+
+			fmt.Println("qu:" + data.Message)
+			fmt.Println("re:" + s)
+
+			re, _ := json.Marshal(toQQ{Message_type: "private", User_id: data.User_id, Message: s})
+			http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
 		}
 	}()
 }
