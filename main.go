96bd9e4c841e7d3c4f5084d24e8ee31454c7a3fa
opsEpic <130291335+opsEpic@users.noreply.github.com>
2023-04-10 13:48:47 +0800

    1.-help显示指令列表 2.-net控制联网 3.-set修正 4.-refresh

--- main.go
+++ main.go
@@ -11,9 +11,21 @@ import (
 	"net/http"
 	"strings"
 	"sync"
+	"time"
 )
 
-var character sync.Map
+type globalSetting struct {
+	apiKey string
+}
+type userSetting struct {
+	system  string
+	netWork bool
+	id      int
+}
+
+var global globalSetting
+var users sync.Map
+
 var logger *logrus.Logger
 
 func myTrim(s string, cut string) string {
@@ -33,20 +45,50 @@ func myTrim(s string, cut string) string {
 	return s
 }
 
-func send2gpt3method1(s string, id int) string {
-	type req struct {
-		Network bool   `json:"network"`
-		Prompt  string `json:"prompt"`
-		UserId  string `json:"userId"`
-		Apikey  string `json:"apikey"`
+func myTrimWithSpace(s string, cut string) string {
+	if strings.Contains(s, cut+" ") {
+		return myTrim(s, cut+" ")
 	}
 
-	w := req{false, s, "#/chat/" + fmt.Sprint(id), "8cbb290c-2c7f-44ef-9d14-df2110319da8"}
+	return myTrim(s, cut)
+}
+
+func send2gpt3method1(s string, id int) string {
+	type req struct {
+		Prompt         string `json:"prompt"`
+		UserId         string `json:"userId"`
+		Network        bool   `json:"network"`
+		Apikey         string `json:"apikey"`
+		System         string `json:"system"`
+		WithoutContext bool   `json:"withoutContext"`
+	}
+
+	w := req{
+		Prompt:         s,
+		UserId:         "#/chat/",
+		Network:        false,
+		Apikey:         global.apiKey,
+		System:         "",
+		WithoutContext: false,
+	}
+	v, ok := users.Load(id)
+	temp := userSetting{id: int(time.Now().UnixMilli())}
+	if ok {
+		temp = v.(userSetting)
+	}
+	users.Store(id, temp)
+
+	w.System = temp.system
+	w.Network = temp.netWork
+	w.UserId += fmt.Sprint(temp.id)
+
 	j, err := json.Marshal(w)
 	if err != nil {
 		logger.Errorln(err, w)
 		return err.Error()
 	}
+
+	fmt.Println(string(j))
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
@@ -90,69 +132,96 @@ func receive(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(ans, &data)
 
 	go func() {
-		if data.Message_type == "group" {
-			if strings.Contains(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"] ") {
-				data.Message = myTrim(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"] ")
-			} else if strings.Contains(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]") {
-				data.Message = myTrim(data.Message, "[CQ:at,qq="+fmt.Sprint(data.Self_id)+"]")
-			} else {
+		messageType := data.Message_type
+		chatId := data.User_id
+		fromId := data.User_id
+		myId := data.Self_id
+		message := data.Message
+
+		s := ""
+
+		if messageType == "group" {
+			if !strings.Contains(message, "[CQ:at,qq="+fmt.Sprint(myId)+"]") {
 				return
 			}
+			message = myTrimWithSpace(message, "[CQ:at,qq="+fmt.Sprint(myId)+"]")
 
-			s := "[CQ:at,qq=" + fmt.Sprint(data.User_id) + "] "
-			if strings.Contains(data.Message, "-set ") {
-				data.Message = myTrim(data.Message, "-set ")
-				character.Store(data.Group_id, data.Message)
-				return
-			} else if strings.Contains(data.Message, "-show") {
-				v, ok := character.Load(data.Group_id)
-				if ok {
-					s += v.(string)
-				}
+			s += "[CQ:at,qq=" + fmt.Sprint(fromId) + "] "
+
+			chatId = data.Group_id
+		}
+
+		if strings.Contains(message, "-help") {
+			s += `帮助：
+-help 显示帮助
+			
+设置当前会话场景：
+-set 设置提示词
+-show 显示提示词
+-net 联网/断网
+-refresh 重置当前情景
+（有的时候提示词会失效，请refresh重置，
+refresh后不需要重新设定提示词）
+			
+设置全局：
+-setkey 设置apikey
+-showkey 显示apikey前后4位
+-showtoken 显示余额
+-showkey 显示apikey`
+
+		} else if strings.Contains(message, "-set") {
+			v, ok := users.Load(chatId)
+			temp := userSetting{}
+			if ok {
+				temp = v.(userSetting)
+			}
+			temp.system = myTrimWithSpace(message, "-set")
+			users.Store(chatId, temp)
+
+			s += "set提示词成功！"
+		} else if strings.Contains(message, "-show") {
+			v, ok := users.Load(chatId)
+			if ok {
+				s += v.(userSetting).system
+			}
+		} else if strings.Contains(message, "-net") {
+			v, ok := users.Load(chatId)
+
+			temp := userSetting{}
+			if ok {
+				temp = v.(userSetting)
+			}
+
+			temp.netWork = !temp.netWork
+			if temp.netWork {
+				s += "联网功能已打开"
 			} else {
-				v, ok := character.Load(data.Group_id)
-				if ok {
-					data.Message += v.(string)
-				}
-				s += send2gpt3method1(data.Message, data.Group_id)
+				s += "联网功能已关闭"
+			}
+			users.Store(chatId, temp)
+		} else if strings.Contains(message, "-refresh") {
+			v, ok := users.Load(chatId)
+
+			temp := userSetting{}
+			if ok {
+				temp = v.(userSetting)
 			}
 
-			logger.Infoln("qu:"+data.Message, data.Group_id, data.User_id)
-			logger.Infoln("re:" + s)
+			temp.id = int(time.Now().UnixMilli())
+			users.Store(chatId, temp)
 
-			re, _ := json.Marshal(toQQ{Message_type: "group", Group_id: data.Group_id, Message: s})
-			_, err := http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
-			if err != nil {
-				logger.Errorln(err, w)
-			}
+			s += "重置情景成功（-set的提示词不会被重置）"
+		} else {
+			s += send2gpt3method1(message, chatId)
+		}
 
-		} else if data.Message_type == "private" {
-			s := ""
-			if strings.Contains(data.Message, "-set ") {
-				data.Message = myTrim(data.Message, "-set ")
-				character.Store(data.User_id, data.Message)
-				return
-			} else if strings.Contains(data.Message, "-show") {
-				v, ok := character.Load(data.User_id)
-				if ok {
-					s += v.(string)
-				}
-			} else {
-				v, ok := character.Load(data.User_id)
-				if ok {
-					data.Message += v.(string)
-				}
-				s += send2gpt3method1(data.Message, data.User_id)
-			}
+		logger.Infoln("qu:"+message, chatId, fromId)
+		logger.Infoln("re:" + s)
 
-			logger.Infoln("qu:"+data.Message, data.User_id)
-			logger.Infoln("re:" + s)
-
-			re, _ := json.Marshal(toQQ{Message_type: "private", User_id: data.User_id, Message: s})
-			_, err := http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
-			if err != nil {
-				logger.Errorln(err, w)
-			}
+		re, _ := json.Marshal(toQQ{Message_type: messageType, User_id: chatId, Group_id: chatId, Message: s})
+		_, err := http.Post(`http://127.0.0.1:5700/send_msg`, "application/json", bytes.NewReader(re))
+		if err != nil {
+			logger.Errorln(err, w)
 		}
 	}()
 }
@@ -160,6 +229,9 @@ func receive(w http.ResponseWriter, r *http.Request) {
 func main() {
 	logger = log.InitLog()
 	logger.Infoln("GPT Bot Start")
+
+	global.apiKey = "8cbb290c-2c7f-44ef-9d14-df2110319da8"
+
 	http.HandleFunc("/", receive)
 	http.ListenAndServe(":5701", nil)
 }
