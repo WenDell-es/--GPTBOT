1816a0bc7c89ea0b82e126e00ce235426ab086cf
wendell-es <1340626169@qq.com>
2023-06-25 00:42:11 +0800

    1.添加新老婆 2.新增老婆作品出处

--- plugin/wife/main.go
+++ plugin/wife/main.go
@@ -2,17 +2,22 @@
 package wife
 
 import (
-	"github.com/sirupsen/logrus"
-	"os"
-	"strings"
-
+	"encoding/json"
 	fcext "github.com/FloatTech/floatbox/ctxext"
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
 	"github.com/FloatTech/zbputils/ctxext"
+	"github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
+	"os"
+	"strings"
 )
+
+type wife struct {
+	Name   string
+	Source string
+}
 
 func init() {
 	engine := control.Register("wife", &ctrl.Options[*zero.Ctx]{
@@ -22,11 +27,9 @@ func init() {
 		PublicDataFolder: "Wife",
 	}).ApplySingle(ctxext.DefaultSingle)
 	_ = os.MkdirAll(engine.DataFolder()+"wives", 0755)
-
 	engine.OnFullMatch("抽老婆").SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			cards := []string{}
-			dirs, err := os.ReadDir(engine.DataFolder() + "wives")
+			data, err := os.ReadFile(engine.DataFolder() + "wife.json")
 			if err != nil {
 				logrus.Errorln(err)
 				ctx.SendChain(
@@ -35,28 +38,36 @@ func init() {
 				)
 				return
 			}
-			for _, dir := range dirs {
-				cards = append(cards, dir.Name())
-			}
-			card := cards[fcext.RandSenderPerDayN(ctx.Event.UserID, len(cards))]
-			data, err := os.ReadFile(engine.DataFolder() + "wives/" + card)
-			card, _, _ = strings.Cut(card, ".")
+			var cards []wife
+			err = json.Unmarshal(data, &cards)
 			if err != nil {
 				logrus.Errorln(err)
 				ctx.SendChain(
 					message.At(ctx.Event.UserID),
-					message.Text("今天的二次元老婆是~【", card, "】哒\n【图片下载失败: ", err, "】"),
+					message.Text("喵喵喵！老婆池加载失败了喵~~！", err),
+				)
+				return
+			}
+
+			card := cards[fcext.RandSenderPerDayN(ctx.Event.UserID, len(cards))]
+			data, err = os.ReadFile(engine.DataFolder() + "wives/" + card.Name)
+			wifeName, _, _ := strings.Cut(card.Name, ".")
+			if err != nil {
+				logrus.Errorln(err)
+				ctx.SendChain(
+					message.At(ctx.Event.UserID),
+					message.Text("今天的二次元老婆是~【", wifeName, "】哒\n【图片下载失败: ", err, "】"),
 				)
 				return
 			}
 			if id := ctx.SendChain(
 				message.At(ctx.Event.UserID),
-				message.Text("今天的二次元老婆是~【", card, "】哒"),
+				message.Text("今天的二次元老婆是~【", wifeName, "】哒!\n来自作品【", card.Source, "】哦~"),
 				message.ImageBytes(data),
 			); id.ID() == 0 {
 				ctx.SendChain(
 					message.At(ctx.Event.UserID),
-					message.Text("今天的二次元老婆是~【", card, "】哒\n【图片发送失败, 请联系维护者】"),
+					message.Text("今天的二次元老婆是~【", wifeName, "】哒\n【图片发送失败, 请联系维护者】"),
 				)
 			}
 		})
